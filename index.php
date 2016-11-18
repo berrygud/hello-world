03b2749a51297c146cbe128bbad16729cd0596a5
berrygud <cherry.villaruz@gmail.com>
2016-11-18 10:43:41 +0800

    Create index.php

---
+++ index.php
@@ -0,0 +1,185 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.0 Transitional//EN">
+<html lang="en" xml:lang="en" xmlns="http://www.w3.org/1999/xhtml">
+<head xmlns="">
+<title>Schools Division of Capiz</title>
+<meta content="text/html; charset=utf-8" http-equiv="Content-Type" />
+<meta name="Keywords" content="">
+<meta name="Description" content="Schools Division of Capiz">
+<meta content="en" http-equiv="Content-Language" />
+<meta name="title" content="Schools Division of Capiz" />
+<meta name="Copyright" content="Schools Division of Capiz">
+<meta name="author" content="CVillaruz-Aminoso">
+<link rel="shortcut icon" href="images/favicon.ico" />
+<link rel="icon" href="images/favicon.ico" />
+<link rel="stylesheet" type="text/css" href="css/style.css" media="all" />
+<link rel="stylesheet" type="text/css" href="css/header.css" media="all" />
+<link rel="stylesheet" type="text/css" href="css/modules.css" media="all" />
+<link rel="stylesheet" type="text/css" href="css/content.css" media="all" />
+<link rel="stylesheet" type="text/css" href="css/footer.css" media="all" />
+<link rel="stylesheet" type="text/css" href="css/breadcrumbs.css" media="all" />
+<link rel="stylesheet" type="text/css" href="css/heros.css" media="all" />
+<link rel="stylesheet" type="text/css" href="css/navigation_side.css" media="all" />
+<link rel="stylesheet" type="text/css" href="css/navigation_top.css" media="all" />
+<link rel="stylesheet" type="text/css" href="css/news.css" media="all" />
+<link rel="stylesheet" type="text/css" href="css/pagination.css" media="all" />
+<link rel="stylesheet" type="text/css" href="css/search.css" media="all" />
+<script type="text/javascript" language="javascript" src="js/load.js" xml:space="preserve"></script>
+
+</head>
+<body xmlns="" linkifymax="0" linkifycurrent="0" linkifying="true">
+<table border="0" align="center" cellpadding="0" cellspacing="0" >
+<tr>
+<td class="hdr_row"><?php include('includes/header.php');?>
+</td>
+</tr>
+<tr style="background-color:f8f5eb;">
+<td >&nbsp;</td>
+</tr>
+<tr class="nt_row">
+<td >
+<!-- topnavigation-->
+ <?php /* $nav = 1; include('includes/topnav.php'); */?>
+</td>
+</tr>
+<tr>
+<td><table cellspacing="0" style="table-layout: fixed;">
+    <tbody>
+      <tr>
+        <td colspan="1" rowspan="1" class="welcome_cell"><table class="ns content welcome" id="welcome" style="height: 210px;">
+            <tbody>
+              <tr>
+                <td rowspan="1" colspan="3">&nbsp;</td>
+              </tr>
+              <tr >
+                <td colspan="2" rowspan="1">
+                  <h3 align="center">Message From Superintendent</h3>
+                  <p class="welcome_content">
+                   <img src="images/sdo_mac_aposin.png" alt=" Miguel Mac D. Aposin" title=" Miguel Mac D. Aposin" style="padding-right:5px; padding-top:5px;" align="left" />We dream of Filipinos who passionately love their country 
+and whose values and competencies enable them to realize their full potential 
+and contribute meaningfully to building the nation.<br><br>As a learner-centered public institution, the Department of Education 
+continuously improves itself to better serve its stakeholders. </p></td>
+                <td colspan="1" rowspan="1">&nbsp;</td>
+              </tr>
+              <tr>
+                <td rowspan="1" colspan="2" class="ns_footer_left">&nbsp;</td>
+                <td colspan="1" rowspan="1" class="ns_footer_right">&nbsp;</td>
+              </tr>
+            </tbody>
+        </table></td>
+        <td class="hero_home_cell"><div>
+             <!--<div style="background: rgb(255, 255, 255) none repeat scroll 0% 50%; -moz-background-clip: -moz-initial; -moz-background-origin: -moz-initial; -moz-background-inline-policy: -moz-initial;"> -->
+             <a href="#"><img src="images/banner_Comprehensive-Tobacco-Control.jpg" border="0" title=""/></a>
+            </div>
+       
+          </div></td>
+          <td class="hero_home_cell">
+          <div  style="padding-left:140px; padding-top:5px; padding-right:5px; padding-bottom:5px;">
+          <h4>What's Happening</h4>
+          <ul>
+<li>2 November 2016 - Resumption of Classes</li>
+<li>3-4 November 2016 - Second Quarter Examinations </li>
+<li>22 December 2016 - Start of Christmas Break </li>  
+<li>25 December 2016 - Christmas Day  </li>
+<li>30 December 2016 - Rizal Day</li>
+          </ul> >> <a href="#">view calendar</a></div></td>
+      </tr>
+      <tr>
+        <td rowspan="1" colspan="2"><table cellspacing="0">
+            <tbody>
+              <tr>
+                <td colspan="1" rowspan="1" class="red_module_cell"><table cellspacing="0" class="red_module">
+                    <tbody>
+                      <tr>
+                        <td colspan="1" rowspan="1" class="red_module_top_left"></td>
+                        <td colspan="1" rowspan="1" class="red_module_top"></td>
+                        <td colspan="1" rowspan="1" class="red_module_top_right"></td>
+                      </tr>
+                      <tr>
+                        <td colspan="1" rowspan="1" class="red_module_left"></td>
+                        <td colspan="1" rowspan="1"><table width="100%" cellspacing="0" cellpadding="0" border="0">
+                            <tbody>
+                              <tr>
+                                <td colspan="1" rowspan="1" class="thumbnail"><a href="#" shape="rect" ><img src="images/ApplyingArrowFeature.gif" alt="future-students.gif" width="50" height="48" class="cms_image" title="" /></a></td>
+                                <td colspan="1" rowspan="1"><h3><a href="#" class="" shape="rect" alt="" title="">Briones: Culture, human rights vital to make Filipinos citizens of the world</a></h3>
+                                  <p>
+Department of Education (DepEd) Secretary Leonor Magtolis Briones recently emphasized the need to enable and empower the Filipinos, particularly the youth, to become citizens of the world through quality, accessible, relevant, and liberating basic education... <a href="#" shape="rect" title="Future Students" class="" alt="Future Students"><img src="images/arrow_red.gif" alt="Future Students" width="10" height="10" border="0" title="Future Students" /></a></p><br><br></td>
+                              </tr>
+                            </tbody>
+                          </table></td>
+                        <td colspan="1" rowspan="1" class="red_module_right"></td>
+                      </tr>
+                      <tr>
+                        <td colspan="1" rowspan="1" class="red_module_left"></td>
+                        <td colspan="1" rowspan="1"><table width="100%" cellspacing="0" cellpadding="0" border="0">
+                            <tbody>
+                              <tr>
+                                <td class="thumbnail" rowspan="1" colspan="1"><a shape="rect" class="" href="#"><img src="images/ApplyingMouseFeature.gif" alt="DepEd assures SHS voucher for Grade 11 in 2017" width="50" height="48" class="cms_image" title="DepEd assures SHS voucher for Grade 11 in 2017" /></a></td>
+                                <td rowspan="1" colspan="1"><h3><a class="" shape="rect" href="#">DepEd assures SHS voucher for Grade 11 in 2017</a></h3>
+                                  <p>
+The Department of Education (DepEd) maintained that the Senior High School (SHS) Voucher Program will remain to provide financial assistance for qualified Grade 10 completers who wish to enroll in non-DepEd SHS amid reports claiming that the voucher program....<a class="" title="New Students" shape="rect" href="#"><img src="images/arrow_red.gif" width="10" height="10" alt="" title="" /></a></p></td>
+                              </tr>
+                            </tbody>
+                          </table>
+                          <p>&nbsp;</p></td>
+                        <td colspan="1" rowspan="1" class="red_module_right"></td>
+                      </tr>
+                      <tr>
+                        <td colspan="1" rowspan="1" class="red_module_bottom_left"></td>
+                        <td colspan="1" rowspan="1" class="red_module_bottom"></td>
+                        <td colspan="1" rowspan="1" class="red_module_bottom_right"></td>
+                      </tr>
+                    </tbody>
+                  </table></td>
+                <td class="latest_discoveries_cell"><table cellspacing="0" class="tan_module">
+                    <tbody>
+                      <tr>
+                        <td colspan="1" rowspan="1" class="tan_module_top_left"></td>
+                        <td colspan="1" rowspan="1" class="tan_module_top"></td>
+                        <td colspan="1" rowspan="1" class="tan_module_top_right"></td>
+                      </tr>
+                      <tr>
+                        <td colspan="1" rowspan="1" class="tan_module_left"></td>
+                        <td colspan="1" rowspan="1"><h2 class="latest_discoveries_heading"><a href="#" class="" shape="rect" alt="Latest news" title="Announcements">Announcements</a></h2>
+                          <table border="0" width="100%" class="tan_module" cellpadding="0" cellspacing="0">
+                            <tr>
+                               <td class="latest_discoveries_thumbnail" valign="top" style="width: 50px;" rowspan="1" colspan="1"><img width="50" height="48" src="images/NewsFeature.gif" alt="Latest News" /></td>
+                              <td align="left" class="latest_discoveries_content" rowspan="1" colspan="1">
+                              <h3><a href="#" title="Briones clarifies DepEd position on 'Oplan Tokhang'">Briones clarifies DepEd position on 'Oplan Tokhang'</a><br>11 Nov 2016</h3>
+                                <p class="content">Department of Education (DepEd) Secretary Leonor Magtolis Briones made clear recently that all schools will not be subjected to the Philippine National Police’s (PNP) "Oplan Tokhang."  <a href="#" shape="rect"><img src="images/bullet_tan.gif" width="11" height="14" border="0" class="bottom" /></a></p><br><br> </td>
+                              
+                            </tr>
+                            <tr>
+                             
+                             <td class="latest_discoveries_thumbnail" valign="top" style="width: 50px;" rowspan="1" colspan="1"><img width="50" height="48" src="images/NewsFeature.gif" alt="Latest News" /></td>
+                              <td align="left" class="latest_discoveries_content" rowspan="1" colspan="1">
+                              <h3><a href="#" title="JICA and DepEd co-host conference to promote stronger partnership between senior high schools and industry">JICA and DepEd co-host conference to promote stronger partnership between senior high schools and industry</a><br>13 Nov 2016</h3>
+                                <p class="content">The Department of Education (DepEd) and the Japan International Cooperation Agency (JICA), through Project for Supporting Senior High School (SHS) Program in Technical Vocational High Schools, co-hosted the Industry Summit from November 7 to 9, in Pasig City <a href="#" shape="rect"><img src="images/bullet_tan.gif" alt="&gt;" width="11" height="14" border="0" class="bottom" /></a></p></td>
+                             
+                             
+                            </tr>
+                        </table></td>
+                        <td colspan="1" rowspan="1" class="tan_module_right"></td>
+                      </tr>
+                      <tr>
+                        <td colspan="1" rowspan="1" class="tan_module_bottom_left"></td>
+                        <td colspan="1" rowspan="1" class="tan_module_bottom"></td>
+                        <td colspan="1" rowspan="1" class="tan_module_bottom_right"></td>
+                      </tr>
+                    </tbody>
+                  </table></td>
+               
+                  </tr>
+                  </table>
+                </td>
+                <td><br><br><a href="http://lis.deped.gov.ph/uis/login" target="_blank"><img src="images/button_LIS.png" alt="Learner Information System" width="300" height="50" title="Learner Information System" /></a><br><br>
+        <a href="http://lrmds.deped.gov.ph/" target="_blank"><img src="images/button_IS-LRMDS.png" alt="Learning Resource Management and Development System" width="300" height="50" title="Learning Resouce Management and Development System"/></a><br><br><a href="http://ebeis.deped.gov.ph/beis/" target="_blank"><img src="images/button_IS-EBEIS.png" alt="Basic Education Information System (BEIS)" width="300" height="50" title="Enhanced Basic Education Information System" border="0" /></a></td>
+              </tr>
+            </tbody>
+          </table></td>
+      </tr>
+<tr>
+<td colspan="1" rowspan="1" class="bottom"><?php include('includes/footer.php');?></td>
+</tr>
+</table>
+</body>
+</html>
